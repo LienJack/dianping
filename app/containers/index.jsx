02966ee3cd198ad8fc93187bb2c6c22f9ca9e933
lien <longyuhuhao@126.com>
2019-05-20 08:39:06 +0800

    1update

--- app/containers/index.jsx
+++ app/containers/index.jsx
@@ -5,12 +5,32 @@ class App extends React.Component {
     constructor(props, context) {
         super(props, context);
         this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+        this.state = {
+          initDone: false
+        }
     }
+    componentDidMount() {
+      setTimeout(() => {
+        this.setState({
+          initDone: true
+        })
+      }, 1000);
+    }
+    
     render() {
+        // return (
+        //     <div>
+        //         {this.props.children}
+        //     </div>
+        // )
         return (
-            <div>
-                {this.props.children}
-            </div>
+          <div>
+            {
+              this.state.initDone 
+              ? this.props.children 
+              : <div>加载中.....</div>
+            }
+          </div>
         )
     }
 }
